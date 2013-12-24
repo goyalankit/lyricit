8be3ea46397064343a01bfc84b238c6a6ebd0855
Ankit Goyal <ankit3goyal@gmail.com>
2013-12-23 19:55:34 -0600

    requiring faraday

--- lib/lyricit.rb
+++ lib/lyricit.rb
@@ -2,8 +2,7 @@ require "lyricit/version"
 require 'lyricit/itunes'
 require 'lyricit/lyrics_api'
 require 'lyricit/make_it_personal'
-require 'bundler'
-Bundler.require
+require 'faraday'
 module Lyricit
   # Your code goes here...
 end
