58e337d9bf3911bf2c80050bb7b0f556b04928da
Ankit Goyal <ankit3goyal@gmail.com>
2013-12-23 20:53:28 -0600

    adding more information for iser

--- lib/lyricit/make_it_personal.rb
+++ lib/lyricit/make_it_personal.rb
@@ -1,8 +1,8 @@
 module Lyricit
     module MakeItPersonal
         def self.get_lyrics name, artist
+            print "Getting Lyrics for:\nSong: #{name.strip}\nArtist: #{artist}"
             return :incomplete_info if name.strip == "" || artist.strip == ""
-
             res = Faraday.get("http://makeitpersonal.co/lyrics?artist=#{artist.strip}&title=#{name.strip}")
 
             return nil if res.status.to_i != 200
